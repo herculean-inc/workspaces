71bffd0e37aa02ae29d0516034c1a9b70f27fc61
Christian Lewis <ctjl96@gmail.com>
2020-01-30 20:58:49 -0600

    BULK AUTOMATIC COMMIT

--- workspaces-desktop/workspaces-desktop-1.0/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(workspaces-desktop):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="workspaces-desktop" command="/usr/bin/workspaces-desktop"
--- workspaces-desktop/workspaces-desktop-1.0/debian/workspaces-desktop.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the workspaces-desktop package
-#
-0 4	* * *	root	[ -x /usr/bin/workspaces-desktop_maintenance ] && /usr/bin/workspaces-desktop_maintenance
